a8d485e2e56d78cbd5975c013b98570675dc5a8f
ljosef18@student.aau.dk <q!69ZibSUn!NhdL>
2021-02-17 13:38:20 +0100

    Made ais data entry

--- src/api/service/ais_data_service.py
+++ src/api/service/ais_data_service.py
@@ -1,7 +1,12 @@
 import psycopg2
+from typing import List
+
+from model.ais_data_entry import AisDataEntry
 
 
 class AisDataService:
+    dsn = "dbname=ais user=postgres password=password host=db"
+
     def __init__(self):
         self.connection = psycopg2.connect(
             user="postgres",
@@ -17,6 +22,37 @@ class AisDataService:
 
         cursor.execute(query)
         return [AisDataService.__build_dict(cursor, row) for row in cursor.fetchall()]
+
+    def insert_many(self, objects: List[AisDataEntry]):
+        conn = None
+        try:
+            conn = psycopg2.connect(self.dsn)
+            cur = conn.cursor()
+            # execute 1st statement
+            count = 0
+            for ais_data_entry in objects:
+                count = count + 1
+                if count > 100:
+                    break
+                cur.execute(
+                    """INSERT INTO ais.data (
+                    timestamp, mobile_type, mmsi, latitude, longitude, nav_stat, rot, sog, cog, heading, imo, callsign,
+                    name, ship_type, cargo_type, width, length, position_fixing_device_type, draught, eta,
+                    data_src_type, destination, a, b, c, d) VALUES
+                    (%s, %s, %s, %s, %s, %s, %s, %S, %s, %s, %s, %s,
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """,
+                    ais_data_entry.__dict__.values()
+                )
+                print(f"Executed {ais_data_entry.__dict__.values()}")
+            conn.commit()
+            # close the database communication
+            cur.close()
+        except psycopg2.DatabaseError as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
 
     @staticmethod
     def __build_dict(cursor, row):
