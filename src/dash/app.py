302a785ae00ac6c87afa5da37cf1e86b4bab1ae8
christoffer <cholle18@student.aau.dk>
2021-02-18 15:34:16 +0100

    code cleanup and added options for displaying using input: 1) sample of a timestamp and 2) one specific ship.

--- src/dash/app.py
+++ src/dash/app.py
@@ -19,49 +19,39 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# 3 pillars
-# dash components
-# callbacks
-#
-
-
-
-# Loading data in a Pandas data frame
 # Giving positional col. names
 colnames = ['Date', 'Class', 'MMSI', 'lat', 'lon', 'NavStatus', 'ROT', 'SOG', 'COG', 'Heading', 'IMO', 'Callsign', 'Name', 'ShipType', 'CargoType', 'Width', 'Length', 'TypeOfPosFixingDevice', 'Draught', 'Destination', 'ETA', 'DataSourceType', 'SizeA', 'SizeB', 'SizeC', 'SizeD'] # giving our AIS data some col. names
 
-# Loading data using read_csv and passing in col. names
+# Loading data in a Pandas data frame using data using read_csv and passing in col. names
 ais_data = pd.read_csv("10k_aisdk_20210209.csv", header=None, names=colnames)
 
-# Some test code to use the different data for the components, this can be copied in callback methods and sorted (probably makes more sense).
-#ais_data = ais_data.loc[ais_data['Name'] == "NKT VICTORIA"]
-ais_data = ais_data.loc[ais_data['Date'] == "09/02/2021 01:16:47"]
-#ais_data = ais_data.loc[ais_data['Class'] == "Class B"]
-
-# HTML section sort of with Dash components
+# Dash components section
 app.layout = html.Div([
-    html.H1("P6 AIS Data", style={'text-align': 'center'}),
+    html.H2("P6 AIS Data", style={'text-align': 'center'}),
+    html.H3("Using first 10k rows of dataset aisdk_20210209", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="dummy_input",
+    # Graph
+    html.H3("Graph Testing"),
+    dcc.Dropdown(id="graph_input",
                  options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
+                     {"label": "ship data for 09/02/2021 01:16:47", "value": "timestamp"},
+                     {"label": "ship data for one ship", "value": "one_ship"},
+                     ],
                  multi=False,
-                 value=2015,
+                 value="timestamp",
                  style={'width': "40%"}
                  ),
     html.Div(id='output-graph'),
     html.Br(),
     dcc.Graph(id='my_ais_data', figure={}),
 
-    dcc.Dropdown(id="dummy_input2",
+    # Pie Chart
+    html.H3("Pie Chart Testing"),
+    dcc.Dropdown(id="piechart_input",
                  options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
+                     {"label": "ship types", "value": 2015},
+                     {"label": "cargo types", "value": 2016},
+                     ],
                  multi=False,
                  value=2015,
                  style={'width': "40%"}
@@ -71,6 +61,7 @@ app.layout = html.Div([
     html.Br(),
     dcc.Graph(id='ais_ship_types', figure={}),
 
+    # Upload data
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -93,7 +84,6 @@ app.layout = html.Div([
     html.Div(id='output-data-upload'),
 ])
 
-# Some method for uploading data, ignore for now
 def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
 
@@ -143,27 +133,33 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
             zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
-# Callback method for showing graph. Might not need a callback, I think there are alternatives
-# Using dummy_input for now because it is required in the callbacks
-@app.callback(Output('my_ais_data', 'figure'), Input('dummy_input', 'value'))
-def show_graph(dummy_input):
-    print(dummy_input)
-    # Plotly Express
-    fig = px.scatter_mapbox(ais_data, lat="lat", lon="lon", hover_name="Name", hover_data=['Date', 'Class', 'MMSI', 'NavStatus', 'ROT', 'SOG', 'COG', 'Heading', 'IMO', 'Callsign', 'Name', 'ShipType'],
+@app.callback(Output('my_ais_data', 'figure'), Input('graph_input', 'value'))
+def show_graph(graph_input):
+    print(graph_input)
+
+    # Showing type of graph based on input
+    if(graph_input == "timestamp"):
+        updated_ais_data = ais_data.loc[ais_data['Date'] == "09/02/2021 01:16:47"]
+    elif(graph_input == "one_ship"):
+        updated_ais_data = ais_data.loc[ais_data['Name'] == "NKT VICTORIA"]
+    else:
+        updated_ais_data = ais_data
+
+    fig = px.scatter_mapbox(updated_ais_data, lat="lat", lon="lon", hover_name="Name", hover_data=['Date', 'Class', 'MMSI', 'NavStatus', 'ROT', 'SOG', 'COG', 'Heading', 'IMO', 'Callsign', 'Name', 'ShipType'],
                             color_discrete_sequence=["red"], zoom=3, height=300)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return fig
 
-# Callback method for showing pie chart. Might not need a callback, I think there are alternatives
-# Using dummy_input for now because it is required in the callbacks
-@app.callback(Output('ais_ship_types', 'figure'), Input('dummy_input2', 'value'))
-def show_graph(dummy_input2):
-    # Plotly Express
-    x_ais_data = ais_data # saving it again in case we messing with it
-    fig = px.pie(x_ais_data, values='ShipType', names='Class', title='Ship types')
-    return fig
+# Testing with piechart for now
+@app.callback(Output('ais_ship_types', 'figure'), Input('piechart_input', 'value'))
+def show_piechart(piechart_input):
+    # Showing type of piechart based on input
+    # TODO Implement
 
+    updated_ais_data = ais_data # saving it again in case we messing with it
+    fig = px.pie(updated_ais_data, values='ShipType', names='Class', title='Ship types')
+    return fig
 
 
 if __name__ == '__main__':
