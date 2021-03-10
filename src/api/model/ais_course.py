0501437798e5b08722677b6f33257c1b511df544
LauJosefsen <ljosef18@student.aau.dk>
2021-03-10 10:33:09 +0100

    Codestyle

--- src/api/model/ais_course.py
+++ src/api/model/ais_course.py
@@ -1,7 +1,5 @@
 class AisCourse:
-    def __init__(
-        self, id, mmsi, ais_points
-    ):
+    def __init__(self, id, mmsi, ais_points):
         self.id = id
         self.mmsi = mmsi
         self.ais_points = ais_points
