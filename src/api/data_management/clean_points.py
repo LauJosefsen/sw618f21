e0b31485af5d916d9eac257e7c5f6031bcbc4b21
LauJosefsen <ljosef18@student.aau.dk>
2021-03-10 10:19:43 +0100

    Fixed styling

--- src/api/data_management/clean_points.py
+++ src/api/data_management/clean_points.py
@@ -2,6 +2,11 @@ from model.ais_data_entry import AisDataEntry
 
 
 def is_point_valid(point: AisDataEntry) -> bool:
+    """
+    Returns true if location data of point is valid
+    :param point:
+    :return:
+    """
     longitude = point.longitude
     latitude = point.latitude
 
