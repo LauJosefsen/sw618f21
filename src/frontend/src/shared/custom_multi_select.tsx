4745efc3ad8b79db1aabc812958f10a2bf2c0826
LauJosefsen <ljosef18@student.aau.dk>
2021-04-28 10:10:16 +0200

    Lots of stuff.

---
+++ src/frontend/src/shared/custom_multi_select.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Select, { CommonProps, components, GroupTypeBase, OptionTypeBase } from "react-select";
+
+interface Props {
+    options: { value: string; label: string }[];
+    value: { value: string; label: string }[];
+    setValue: (value: { value: string; label: string }[]) => void;
+}
+
+const ValueContainer = (props: any) => {
+    let count = props.getValue().length;
+    let options_count = props.options.length;
+
+    return (
+        <components.ValueContainer {...props}>
+            <span>{count === 0 ? "Nothing selected" : count <= options_count ? `${count} selected` : "Everything selected"}</span>
+        </components.ValueContainer>
+    );
+};
+
+export const CustomMultiSelect = (props: Props) => {
+    return (
+        <Select
+            components={{ ValueContainer }}
+            value={props.value}
+            onChange={(e: any) => props.setValue(e)}
+            defaultValue={props.options}
+            isMulti
+            name="colors"
+            options={props.options}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            hideSelectedOptions={false}
+            closeMenuOnSelect={false}
+        />
+    );
+};
