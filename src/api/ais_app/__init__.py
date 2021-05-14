625c5141e66319877b2ddfb46103a5220c5092ef
ljosef18@student.aau.dk <q!69ZibSUn!NhdL>
2021-05-14 17:28:38 +0200

    Fixed simplejson getting removed in flask 2.0

--- src/api/ais_app/__init__.py
+++ src/api/ais_app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 from .blueprints import data_management, enc_cells, heatmaps, tracks, depth_map
 from .containers import Container
+from simplejson import JSONEncoder
+
 
 container = Container()
 container.wire(modules=[data_management, enc_cells, heatmaps, tracks, depth_map])
@@ -13,6 +15,7 @@ app.register_blueprint(enc_cells.blueprint, url_prefix="/enc")
 app.register_blueprint(heatmaps.blueprint, url_prefix="/heatmaps")
 app.register_blueprint(tracks.blueprint, url_prefix="/tracks")
 app.register_blueprint(depth_map.blueprint, url_prefix="/depth_map")
+app.json_encoder = JSONEncoder
 
 
 @app.route("/")
